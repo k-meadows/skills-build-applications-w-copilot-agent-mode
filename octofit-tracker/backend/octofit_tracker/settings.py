e45e7a1cd8840ca7110639ceaf5a4a4a01a4ed26
k-meadows <k.meadows@comcast.net>
2026-02-24 21:26:45 +0000

    Setup REST framework and test endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -27,7 +27,14 @@ SECRET_KEY = 'django-insecure-&$le@5i2n_ud5tw^zl(g@gf&eydb-#lw=ds=a1el9_y5l!o!ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+# Allow localhost and codespace URL
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
